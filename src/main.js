aeeff8d9ed45bdf9625d9c213839a14719fc3173
Shriley <1368102573@qq.com>
2020-10-31 16:37:00 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
+/**
+ * 项目的启动入口
+*/
+
+// 加载VUE
 import Vue from 'vue'
+// 加载根组件
 import App from './App.vue'
+// 加载路由
 import router from './router'
+// 加载全局样式文件
+import './styles/index.less'
 
+// 关闭生产环境提示
 Vue.config.productionTip = false
-
+// 创建 VUE 根实例
 new Vue({
+  // 把 router 配置到根实例中
   router,
+  // 通过 render 方法把 App 根组件渲染到 #app 入口节点
   render: h => h(App)
+  // el:'#app'// 等价于$mount('#app')
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,17 @@
+// 加载VUE
 import Vue from 'vue'
+// 加载VueRouter
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 配置路由
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 路由配置表
+const routes = []
 
+// 新建VueRouter的实例
 const router = new VueRouter({
   routes
 })
-
+// 导出实例
 export default router
